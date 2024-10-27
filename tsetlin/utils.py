daf05841fc7dfb31a7b518820042f15ed66a09fc
Toby O'Donoghue <tobyrcod@gmail.com>
2024-10-27 12:47:37 +0000

    Add util static method to load tournament data from a results file

---
+++ tsetlin/utils.py
@@ -0,0 +1,78 @@
+import sys
+from pathlib import Path
+from pysgf import SGF
+
+class TournamentUtils:
+
+    @staticmethod
+    def load_tournament_games(tournament_path: Path):
+        result_keys = ['GAME', 'ROUND', 'OPENING', 'BLACK', 'WHITE', 'RES_B', 'RES_W', 'LENGTH', 'TIME_B', 'TIME_W', 'ERR', 'ERR_MSG']
+
+        try:
+            games = []
+            game_boardsize = None
+
+            results_path = tournament_path / "results"
+            with results_path.open('r') as results_file:
+                for line_r in results_file:
+                    if not line_r:
+                        continue
+
+                    # Get the boardsize used for each sgf file
+                    if line_r[0] == '#':
+                        if not game_boardsize and 'Boardsize' in line_r:
+                            game_boardsize = int(line_r.split(':')[1].strip())
+                        continue
+
+                    # Parse the result line into values
+                    result = line_r.strip().split(sep='\t', maxsplit=11)
+                    result = dict(zip(result_keys, result))
+
+                    # Perform metadata results checks
+                    if result['ERR'] == '1':                                                            # The game isn't complete
+                        continue
+                    if result['RES_B'] != result['RES_W']:                                              # Players need to agree on a winner
+                        continue
+                    if result['RES_B'] not in ("B+", "W+"):                                             # The winner must be a player (may be '?' if game isn't over)
+                        continue
+                    game_winner = result['RES_B']
+
+                    # We need to open the sgf file for the game history matching this result
+                    sgf_path = tournament_path / f"{result["GAME"]}.sgf"
+                    sgf = SGF.parse_file(sgf_path)
+
+                    # Perform metadata game checks
+                    if int(sgf.get_property('GM')) != 11:                                               # GM[11] means the game of Hex
+                        continue
+                    if sgf.get_property("US") != "twogtp.py":                                           # Need sgf files to be in the right format
+                        continue
+                    if sgf.get_property('RE') != game_winner:                                           # We need the right winner for this game
+                        continue
+                    if any(n != game_boardsize for n in sgf.board_size):                                # Matching boardsize
+                        continue
+
+                    # Convert the sgf move tree into a list
+                    game_moves = []
+                    move_node = sgf.children[0]
+                    while move_node:
+                        player = move_node.player
+                        game_moves.append(move_node.get_property(player))
+
+                        children = move_node.children
+                        move_node = move_node.children[0] if children else None
+                    game_moves = game_moves[1:-1]  # First node in tree is metadata, last node is "resign"
+
+                    # Save the game
+                    games.append((game_winner, game_moves))
+
+            return games
+
+        except (FileNotFoundError, NotADirectoryError) as e:
+            print(e, file=sys.stderr)
+
+
+if __name__ == "__main__":
+    # Test tournament loader with default path
+    games = TournamentUtils.load_tournament_games(Path("test_tournament/6x6-mohex-mohex-simple-a-vs-mohex-mohex-simple-b"))
+    print(games)
+    print(len(games))
