f9c31461df97dd1af8a75af2165e52953f2f8e16
Toby O'Donoghue <tobyrcod@gmail.com>
2024-10-27 18:50:33 +0000

    Load winner prediction dataset and prepare for batch TM training - Also added boardsize to the output dataset as metadata

--- tsetlin/test.py
+++ tsetlin/test.py
@@ -1,25 +1,25 @@
-from utils import Utils
+from utils import UtilsBenzene
 
 class Tests:
 
     @staticmethod
     def test_hex_string_to_index():
-        assert Utils._hex_string_to_number('a') == 1
-        assert Utils._hex_string_to_number('b') == 2
-        assert Utils._hex_string_to_number('z') == 26
-        assert Utils._hex_string_to_number('aa') == 27
-        assert Utils._hex_string_to_number('ab') == 28
-        assert Utils._hex_string_to_number('az') == 52
-        assert Utils._hex_string_to_number('aaa') == 703
+        assert UtilsBenzene._hex_string_to_number('a') == 1
+        assert UtilsBenzene._hex_string_to_number('b') == 2
+        assert UtilsBenzene._hex_string_to_number('z') == 26
+        assert UtilsBenzene._hex_string_to_number('aa') == 27
+        assert UtilsBenzene._hex_string_to_number('ab') == 28
+        assert UtilsBenzene._hex_string_to_number('az') == 52
+        assert UtilsBenzene._hex_string_to_number('aaa') == 703
 
 
     @staticmethod
     def test_hex_coord_to_index():
-        assert Utils._hex_position_to_index('a1', 6) == 0
-        assert Utils._hex_position_to_index('f1', 6) == 5
-        assert Utils._hex_position_to_index('d2', 6) == 9
-        assert Utils._hex_position_to_index('f6', 6) == 35
-        assert Utils._hex_position_to_index('g1', 6) == -1
+        assert UtilsBenzene._hex_position_to_index('a1', 6) == 0
+        assert UtilsBenzene._hex_position_to_index('f1', 6) == 5
+        assert UtilsBenzene._hex_position_to_index('d2', 6) == 9
+        assert UtilsBenzene._hex_position_to_index('f6', 6) == 35
+        assert UtilsBenzene._hex_position_to_index('g1', 6) == -1
 
 
 if __name__ == "__main__":
