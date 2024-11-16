dd023555e88a649da172de75cd6bc9970a8408c5
Toby O'Donoghue <tobyrcod@gmail.com>
2024-11-16 17:39:26 +0000

    Add more TM demos

--- tsetlin/tsetlinmachinedemos.py
+++ tsetlin/tsetlinmachinedemos.py
@@ -3,9 +3,15 @@
 ###
 
 from pyTsetlinMachine.tm import MultiClassTsetlinMachine
+from pyTsetlinMachine.tools import Binarizer
 import numpy as np
 
-def multiclass_xor_demo():
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import time
+
+def interpretability_xor_demo():
 	number_of_features = 20
 	noise = 0.1
 
@@ -69,3 +75,48 @@ def multiclass_xor_demo():
 				else:
 					l.append("¬x%d" % (k-number_of_features))
 		print(" ∧ ".join(l))
+
+def continuous_breast_cancer_demo():
+	breast_cancer = datasets.load_breast_cancer()
+	X = breast_cancer.data
+	Y = breast_cancer.target
+
+	b = Binarizer(max_bits_per_feature=10)
+	b.fit(X)
+	X_transformed = b.transform(X)
+
+	tm = MultiClassTsetlinMachine(800, 40, 5.0)
+
+	print("\nMean accuracy over 100 runs:\n")
+	tm_results = np.empty(0)
+	for i in range(100):
+		X_train, X_test, Y_train, Y_test = train_test_split(X_transformed, Y, test_size=0.2)
+
+		tm.fit(X_train, Y_train, epochs=25)
+		tm_results = np.append(tm_results, np.array(100 * (tm.predict(X_test) == Y_test).mean()))
+		print("#%d Average Accuracy: %.2f%% +/- %.2f" % (
+		i + 1, tm_results.mean(), 1.96 * tm_results.std() / np.sqrt(i + 1)))
+
+def mnist_demo():
+	(X_train, Y_train), (X_test, Y_test) = tf.keras.datasets.mnist.load_data()
+
+	X_train = np.where(X_train.reshape((X_train.shape[0], 28 * 28)) > 75, 1, 0)
+	X_test = np.where(X_test.reshape((X_test.shape[0], 28 * 28)) > 75, 1, 0)
+
+	tm = MultiClassTsetlinMachine(2000, 50, 10.0)
+
+	print("\nAccuracy over 250 epochs:\n")
+	for i in range(250):
+		start_training = time.time()
+		tm.fit(X_train, Y_train, epochs=1, incremental=True)
+		stop_training = time.time()
+
+		start_testing = time.time()
+		result = 100 * (tm.predict(X_test) == Y_test).mean()
+		stop_testing = time.time()
+
+		print("#%d Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (
+		i + 1, result, stop_training - start_training, stop_testing - start_testing))
+
+print(tf.__version__)
+mnist_demo()
