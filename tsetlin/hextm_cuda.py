3e35960dbf62d4b858dbe06944845f83065e80ee
Toby O'Donoghue <tobyrcod@gmail.com>
2024-11-30 12:09:11 +0000

    Small improvements to CUDA training

--- tsetlin/hextm_cuda.py
+++ tsetlin/hextm_cuda.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 
 # Define parameter settings
 # from winner prediction paper: Logic-based AI for Interpretable Board Game Winner Prediction with Tsetlin Machine
-tm_number_of_clauses = 10_000
+tm_number_of_clauses = 10000
 tm_T = 8000
 tm_s = 100
 tm_max_weight = 255
@@ -37,6 +37,12 @@ X_train, X_test, Y_train, Y_test = train_test_split(DATASET.X, DATASET.Y,
                                                     stratify=DATASET.Y, random_state=42,
                                                     test_size=tm_train_test_split)
 
+# Get the original accuracy
+tm.fit(X_train[0:1], Y_train[0:1], epochs=1, incremental=True)
+acc_train = 100 * (tm.predict(X_train) == Y_train).mean()
+acc_test = 100 * (tm.predict(X_test) == Y_test).mean()
+print(f"Epoch: {0}, Acc Train: {acc_train:.4f}, Acc Test: {acc_test:.4f}")
+
 # Train the TM
 for i in range(tm_epochs):
     tm.fit(
@@ -47,4 +53,6 @@ for i in range(tm_epochs):
     )
 
     # See how well the TM performed
-    print(f"Epoch: {i+1}, Accuracy: {100 * (tm.predict(X_test) == Y_test).mean()}")
+    acc_train = 100 * (tm.predict(X_train) == Y_train).mean()
+    acc_test = 100 * (tm.predict(X_test) == Y_test).mean()
+    print(f"Epoch: {i+1}, Acc Train: {acc_train:.4f}, Acc Test: {acc_test:.4f}")
