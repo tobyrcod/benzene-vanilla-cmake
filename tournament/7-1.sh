50abce770524d04860a4895b2a0804955e1ed044
Toby O'Donoghue <tobyrcod@gmail.com>
2024-12-19 15:08:57 +0000

    fix directory problems with slurm files

--- tournament/7-1.sh
+++ tournament/7-1.sh
@@ -5,7 +5,7 @@
 
 #SBATCH -p "cpu"
 #SBATCH --qos="short"
-#SBATCH -t 00-00:10:00
+#SBATCH -t 00-00:30:00
 source /etc/profile
 
 ./run-mohex-vs-mohex.sh --openings "openings/7x7-all-1ply" --size 7 --rounds 1 "7x7-settings" "7x7-settings"
