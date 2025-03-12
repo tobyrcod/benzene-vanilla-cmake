132f730b82a6f6f0c63d55c8133e282539f02d67
Toby O'Donoghue <tobyrcod@gmail.com>
2025-03-12 12:42:20 +0000

    Analyse the occurrence of black and white templates in weighted tmu clauses

--- tsetlin/playground.py
+++ tsetlin/playground.py
@@ -1,61 +1,136 @@
+import json
 from pathlib import Path
+from typing import DefaultDict
+
+import numpy as np
+
+from tsetlin.control import WinnerPredModel
 from utils import *
 
-boardsize = 6
+UtilsHex.SearchPattern.initialise()
 
-clauses, weights = UtilsTM.Model.load_trained_tmu_model_clauses(Path("models/tmu/6x6-baseline_exact_8limit/weighted_clauses.json"), boardsize)
+clause_path = Path("models/tmu/6x6-baseline_exact_8limit/weighted_clauses.json")
 
-i = 252
+# Calculate matches
+# print('Finding matches...')
+# UtilsHex.SearchPattern.calculate_matches_in_clauses(clause_path, 6)
 
-UtilsPlot.plot_clause(clauses[i], boardsize, Path("democlause.png"))
+# Reload the clauses & matches
+clauses, clauses_weights, clauses_matches  = UtilsHex.SearchPattern.load_matches_in_clauses(clause_path, 6)
+print(len(clauses), len(clauses_weights), len(clauses_matches))
 
-print(weights[i], clauses[i])
+# Visualise a clause
+# UtilsPlot.plot_literals(clauses[6], 6, clause_path.parent / "plot.png")
+# print(clauses_matches[6])
 
-# -------------------------------------------------------------
-print('------------------------------------------------------')
-# -------------------------------------------------------------
+# Work out the score for the found clauses templates
+# These matrices define scores for the 4 possible discrete cases:
+# [0, 0]: Black Won the Clause and Black Won the Match
+# [0, 1]: Black Won the Clause and White Won the Match
+# [1, 0]: White Won the Clause and Black Won the Match
+# [1, 1]: White Won the Clause and White Won the Match
+discrete_clause_discrete_match_matrix = [[0,0], [0,0]]
+discrete_clause_weighted_match_matrix = [[0,0], [0,0]]
+# These lists define scores for the 2 possible weighted cases:
+# [0, 0]: Black's clause weight total when Black Won the Match
+# [0, 1]: Black's clause weight total when White Won the Match
+# [1, 0]: White's clause weight total when Black Won the Match
+# [1, 1]: White's clause weight total when White Won the Match
+weighted_clause_discrete_match_matrix = [[0,0], [0,0]]
+weighted_clause_weighted_match_matrix = [[0,0], [0,0]]
+# We also just want to track the total number of matches for black and white
+total_player_matches = [defaultdict(int), defaultdict(int)]
+# And the total clause weight used by each player
+total_player_weights = [0, 0]
+total_player_absolute_weights = [0, 0]
 
-def check():
-    ones = 0
-    for j, clause in enumerate(clauses):
-        _, codes = UtilsTM.Model._clean_to_sat_clause(clause)
-        ones += Counter(codes)[1]
-        if 1 in Counter(codes):
-            print('1', j)
-    print(ones)
+# Perform the analysis
+for i in range(len(clauses)):
+    # If this clause has no templates, then we ignore it
+    if i not in clauses_matches:
+        continue
 
-check()
+    # This clause has a template, so we want to know more about it
+    clause = clauses[i]
+    weights = clauses_weights[i]
+    matches = clauses_matches[i]
 
-# -------------------------------------------------------------
-print('------------------------------------------------------')
-# -------------------------------------------------------------
+    # Save information about the total weights we have seen
+    total_player_weights[0] += weights[0]
+    total_player_weights[1] += weights[1]
+    total_player_absolute_weights[0] += abs(weights[0])
+    total_player_absolute_weights[1] += abs(weights[1])
 
-print('before', len(clauses), len(weights))
-clauses, weights = UtilsTM.Model.make_model_clauses_satisfiable(clauses, weights)
-print('after', len(clauses), len(weights))
+    # We look one by one at each match in the clause
+    for match in matches:
+        # Each match type has its own best found weights for how important to a win it is
+        match_type = match['MatchType']
+        match_type_weight = WinnerPredModel.BEST_TYPE_WEIGHTS[match_type.value-1]
+        match_type_discrete = -1 if match_type == UtilsHex.SearchPattern.Match.MatchType.LOST else 1
 
-branch_factors = list(map(UtilsTM.Model.calculate_clause_branch_factor, clauses))
+        # This match is either found for black or white
+        match_winner = match['MatchPlayer']
+        total_player_matches[match_winner][match_type.name] += 1
 
-print('upper bound total bf', sum(branch_factors))
+        # Discrete Clause Analysis
+        discrete_clause_winner = np.argmax(weights)
+        discrete_clause_discrete_match_matrix[discrete_clause_winner][match_winner] += 1 * match_type_discrete
+        discrete_clause_weighted_match_matrix[discrete_clause_winner][match_winner] += 1 * match_type_weight
 
-# -------------------------------------------------------------
-print('------------------------------------------------------')
-# -------------------------------------------------------------
+        # Weighted Clause Analysis
+        weighted_clause_discrete_match_matrix[0][match_winner] += weights[0] * match_type_discrete
+        weighted_clause_discrete_match_matrix[1][match_winner] += weights[1] * match_type_discrete
+        weighted_clause_weighted_match_matrix[0][match_winner] += weights[0] * match_type_weight
+        weighted_clause_weighted_match_matrix[1][match_winner] += weights[1] * match_type_weight
 
-def fix():
-    non_negated_clauses, non_negated_weights = [], []
-    for j in range(len(clauses)):
-        clause = clauses[j]
-        weight = weights[j]
+def print_discrete_clause_result_matrix(matrix):
+    print('[0, 0]: Black Won the Clause and Black Won the Match')
+    print(matrix[0][0])
+    print('[0, 1]: Black Won the Clause and White Won the Match')
+    print(matrix[0][1])
+    print('[1, 0]: White Won the Clause and Black Won the Match')
+    print(matrix[1][0])
+    print('[1, 1]: White Won the Clause and White Won the Match')
+    print(matrix[1][1])
 
-        expanded_clauses = UtilsTM.Model.expand_negated_literals_in_clause(clause)
-        for expanded_clause in expanded_clauses:
-            non_negated_clauses.append(expanded_clause)
-            non_negated_weights.append(weight)
+def print_weighted_clause_result_matrix(matrix):
+    print("[0, 0]: Black's clause weight total when Black Won the Match")
+    print(matrix[0][0])
+    print("[0, 1]: Black's clause weight total when White Won the Match")
+    print(matrix[0][1])
+    print("[1, 0]: White's clause weight total when Black Won the Match")
+    print(matrix[1][0])
+    print("[1, 1]: White's clause weight total when White Won the Match")
+    print(matrix[1][1])
 
-    return non_negated_clauses, non_negated_weights
-
-# TODO: move to utils like the rest
-print('before', len(clauses), len(weights))
-clauses, weights = fix()
-print('after', len(clauses), len(weights))
+print('------------------------------')
+print('black total matches')
+print(len(total_player_matches[0]), total_player_matches[0])
+print('white total matches')
+print(len(total_player_matches[1]), total_player_matches[1])
+print('------------------------------')
+print('winning weights %')
+print('black', len([1 for b, w in clauses_weights if b > w]) / len(clauses_weights))
+print('white', len([1 for b, w in clauses_weights if w > b]) / len(clauses_weights))
+print('------------------------------')
+print('negative weights')
+print('black', len([1 for b, w in clauses_weights if b < 0]))
+print('white', len([1 for b, w in clauses_weights if w < 0]))
+print('------------------------------')
+print('black total weight')
+print('sum: ', total_player_weights[0], 'abs: ', total_player_absolute_weights[0], 'avg: ', total_player_weights[0]/len(clauses_weights))
+print('white total weight')
+print('sum: ', total_player_weights[1], 'abs: ', total_player_absolute_weights[1], 'avg: ', total_player_weights[1]/len(clauses_weights))
+print('------------------------------')
+print('discrete clause, discrete match')
+print_discrete_clause_result_matrix(discrete_clause_discrete_match_matrix)
+print('------------------------------')
+print('discrete clause, weighted match')
+print_discrete_clause_result_matrix(discrete_clause_weighted_match_matrix)
+print('------------------------------')
+print('weighted clause, discrete match')
+print_weighted_clause_result_matrix(weighted_clause_discrete_match_matrix)
+print('------------------------------')
+print('weighted clause, weighted match')
+print_weighted_clause_result_matrix(weighted_clause_weighted_match_matrix)
+print('------------------------------')
