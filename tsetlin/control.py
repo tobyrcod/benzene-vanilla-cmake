e48f98b73086c2f8706cdfe07e61fb6c9637b418
Toby O'Donoghue <tobyrcod@gmail.com>
2025-03-20 17:40:26 +0000

    Remove name weight training from heuristic zero shot model

--- tsetlin/control.py
+++ tsetlin/control.py
@@ -21,32 +21,27 @@ class WinnerPredModel:
     # Potentially add turn indicators to influence the weights
 
     # [Lost, Empty, Inconclusive, Won]
-    BEST_TYPE_WEIGHTS = [0.0, 0.13, 0.14, 0.84]
+    # BEST_TYPE_WEIGHTS = [0.0, 0.13, 0.14, 0.84]  # X6_BASELINE
+    BEST_TYPE_WEIGHTS = [-0.06, 0.13, 0.20, 0.96]
 
     @staticmethod
-    def predict_winners_for_dataset(dataset: UtilsDataset.Dataset, name_weights=None, type_weights=None):
+    def predict_winners_for_dataset(dataset: UtilsDataset.Dataset, type_weights=None):
         num_games, matches = UtilsHex.SearchPattern.load_matches_in_dataset(dataset)
         assert num_games == len(dataset.Y)
 
         all_predictions = []
         for (board_number, board_matches) in matches.items():
-            prediction = WinnerPredModel._predict_winner_from_matches(board_matches, name_weights, type_weights)
+            prediction = WinnerPredModel._predict_winner_from_matches(board_matches, type_weights)
             all_predictions.append(prediction)
 
         return all_predictions, set(matches.keys())
 
     @staticmethod
-    def _predict_winner_from_matches(matches: List, name_weights=None, type_weights=None):
+    def _predict_winner_from_matches(matches: List, type_weights=None):
         # Predict the winner of a game purely by a heuristic of the templates in the state
 
         if not matches:
             raise NotImplementedError()
-
-        match_names = UtilsHex.SearchPattern.get_pattern_names()
-        if not name_weights:
-            # TODO: find best weights
-            name_weights = [0] * len(match_names)
-        name_weights = dict(zip(match_names, name_weights))
 
         if not type_weights:
             type_weights = WinnerPredModel.BEST_TYPE_WEIGHTS
@@ -55,9 +50,8 @@ class WinnerPredModel:
         board_score = 0
         for match in matches:
             # TODO: should I add or multiply? - multiply seems to give best results
-            match_name_weight = name_weights.get(match['MatchBaseName'], 0)
             match_type_weight = type_weights.get(match['MatchType'], 0)
-            match_score = match_name_weight * match_type_weight
+            match_score = match_type_weight
 
             player = match['MatchPlayer']
             if player == 1:
@@ -69,53 +63,7 @@ class WinnerPredModel:
         return prediction
 
     @staticmethod
-    def _find_best_name_weights():
-        UtilsDataset.load_raw_datasets()
-        UtilsHex.SearchPattern.initialise()
-        dataset = UtilsDataset.BASELINE
-
-        # We need to find the best weights, use bayesian optimisation to do this
-        space = [Real(low=0, high=1, name="trapezoid"),
-                 Real(low=0, high=1, name="bridge"),
-                 Real(low=0, high=1, name="wheel"),
-                 Real(low=0, high=1, name="span"),
-                 Real(low=0, high=1, name="crescent")]
-
-        # We need to define what we are trying to optimise for
-        # In our case it is accuracy (for example)
-        @use_named_args(space)
-        def objective(**params):
-            name_weights = [params["trapezoid"], params["bridge"], params["wheel"], params["span"], params["crescent"]]
-            y_pred, boards_with_matches = WinnerPredModel.predict_winners_for_dataset(dataset, name_weights, None)
-            y_true = [dataset.Y[i] for i in range(len(dataset.Y)) if i in boards_with_matches]
-
-            report = classification_report(y_true, y_pred, output_dict=True)
-            return -report['accuracy']  # Need to negate accuracy as this process minimises the objective
-
-        result = gp_minimize(
-            objective,
-            space,
-            n_calls=50,
-            acq_func='EI',
-            verbose=True,
-            n_jobs=-1
-        )
-        best_weights = result.x
-        best_accuracy = -result.fun
-        print(f"Best accuracy: {best_accuracy:.4f} with weights {best_weights}")
-
-        plt.plot(-result.func_vals)  # Convert negative accuracy back to positive
-        plt.xlabel("Iteration")
-        plt.ylabel("Accuracy")
-        plt.title("Bayesian Optimization Progress")
-        UtilsPlot.save_plot(plt, Path("optimisation.png"))
-
-    @staticmethod
-    def _find_best_type_weights():
-        UtilsDataset.load_raw_datasets()
-        UtilsHex.SearchPattern.initialise()
-        dataset = UtilsDataset.BASELINE
-
+    def _find_best_type_weights(dataset: UtilsDataset.Dataset):
         # We need to find the best weights, use bayesian optimisation to do this
         space = [Real(low=-1, high=0, name="lost"),
                  Real(low=-0.5, high=0.5, name="empty"),
@@ -127,50 +75,7 @@ class WinnerPredModel:
         @use_named_args(space)
         def objective(**params):
             type_weights = [params["lost"], params["empty"], params["inconclusive"], params["won"]]
-            y_pred, boards_with_matches = WinnerPredModel.predict_winners_for_dataset(dataset, None, type_weights)
-            y_true = [dataset.Y[i] for i in range(len(dataset.Y)) if i in boards_with_matches]
-
-            report = classification_report(y_true, y_pred, output_dict=True)
-            return -report['accuracy']  # Need to negate accuracy as this process minimises the objective
-
-        result = gp_minimize(
-            objective,
-            space,
-            n_calls=50,
-            acq_func='EI',
-            verbose=True,
-            n_jobs=-1
-        )
-        best_weights = result.x
-        best_accuracy = -result.fun
-        print(f"Best accuracy: {best_accuracy:.4f} with weights {best_weights}")
-
-        plt.plot(-result.func_vals)  # Convert negative accuracy back to positive
-        plt.xlabel("Iteration")
-        plt.ylabel("Accuracy")
-        plt.title("Bayesian Optimization Progress")
-        UtilsPlot.save_plot(plt, Path("optimisation.png"))
-
-    @staticmethod
-    def _find_best_combined_weights():
-        # We need to find the best weights, use bayesian optimisation to do this
-        space = ([Real(low=0, high=1, name="trapezoid"),
-                Real(low=0, high=1, name="bridge"),
-                Real(low=0, high=1, name="wheel"),
-                Real(low=0, high=1, name="span"),
-                Real(low=0, high=1, name="crescent")] +
-                 [Real(low=-1, high=0, name="lost"),
-                 Real(low=-0.5, high=0.5, name="empty"),
-                 Real(low=-0.5, high=0.5, name="inconclusive"),
-                 Real(low=0, high=1, name="won")])
-
-        # We need to define what we are trying to optimise for
-        # In our case it is accuracy (for example)
-        @use_named_args(space)
-        def objective(**params):
-            name_weights = [params["trapezoid"], params["bridge"], params["wheel"], params["span"], params["crescent"]]
-            type_weights = [params["lost"], params["empty"], params["inconclusive"], params["won"]]
-            y_pred, boards_with_matches = WinnerPredModel.predict_winners_for_dataset(dataset, name_weights, type_weights)
+            y_pred, boards_with_matches = WinnerPredModel.predict_winners_for_dataset(dataset, type_weights)
             y_true = [dataset.Y[i] for i in range(len(dataset.Y)) if i in boards_with_matches]
 
             report = classification_report(y_true, y_pred, output_dict=True)
@@ -196,17 +101,16 @@ class WinnerPredModel:
 
 
 class WinnerPredGA:
-    NUM_WEIGHTS = 9  # 5 name weights + 4 type weights
+    NUM_WEIGHTS = 4 # 4 type weights
 
     @staticmethod
     def evaluate(individual):
         """
         Evaluates an individual (set of weights) by computing the classification accuracy.
         """
-        name_weights = individual[:5]
-        type_weights = individual[5:]
+        type_weights = individual
 
-        dataset = UtilsDataset.BASELINE
+        dataset = UtilsDataset.X6_BASELINE
         y_pred, boards_with_matches = WinnerPredModel.predict_winners_for_dataset(dataset, name_weights, type_weights)
         y_true = [dataset.Y[i] for i in range(len(dataset.Y)) if i in boards_with_matches]
 
@@ -270,6 +174,7 @@ class WinnerPredGA:
 if __name__ == '__main__':
     UtilsDataset.load_raw_datasets()
     UtilsHex.SearchPattern.initialise()
-    dataset = UtilsDataset.BASELINE
 
-    WinnerPredGA.optimize()
+    dataset = UtilsDataset.X6_EQUAL_UNDER
+    # UtilsHex.SearchPattern.calculate_matches_in_dataset(dataset)
+    WinnerPredModel._find_best_type_weights(dataset)
