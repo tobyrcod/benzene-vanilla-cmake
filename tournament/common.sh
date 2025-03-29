75b967831b3cbb8b488f1e3a2810aabbfa0cb114
Toby O'Donoghue <tobyrcod@gmail.com>
2025-03-29 16:34:06 +0000

    include openings file name in tournament name

--- tournament/common.sh
+++ tournament/common.sh
@@ -55,7 +55,7 @@ run_tournament()
         NAME2=$NAME2"-b"
     fi
 
-    DIRECTORY="jobs/"$SIZE"x"$SIZE"-"$NAME1"-vs-"$NAME2
+    DIRECTORY="jobs/${OPENINGS##*/}-$NAME1-vs-$NAME2"
 
     mkdir -p $DIRECTORY
 
