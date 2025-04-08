726d7ad6dad465beaaf0d1890b92e8de4b71e552
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2025-04-08 19:15:46 +0100

    6x6 datasets sgfs to csv

--- tsetlin/tournaments/tournament_results_to_winner_pred_dataset.py
+++ tsetlin/tournaments/tournament_results_to_winner_pred_dataset.py
@@ -1,7 +1,7 @@
 from tsetlin.utils import UtilsTournament
 from pathlib import Path
 
-from_tournament_path = Path("6x6-3ply-simple")
+from_tournament_path = Path("6/6x6-blunder1")
 to_dataset_path = from_tournament_path / "dataset.csv"
 
 # Loader the tournament results file
@@ -9,4 +9,4 @@ games, boardsize = UtilsTournament.load_tournament_games(from_tournament_path)
 print(games[0])
 
 # Convert it to a winner prediction dataset
-# UtilsTournament.games_to_winner_prediction_dataset(games, boardsize, to_dataset_path)
+UtilsTournament.games_to_winner_prediction_dataset(games, boardsize, to_dataset_path)
